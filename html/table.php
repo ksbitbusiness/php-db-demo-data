c1a34adc159b9bff4e9b202c4fe9971b42680d07
ksbitbusiness <83622223+ksbitbusiness@users.noreply.github.com>
2021-06-07 15:42:52 +0900

    Update table.php

--- html/table.php
+++ html/table.php
@@ -1,1 +1,65 @@
+<!doctype html>
+<html lang="en">
+  <head>
+    <!-- Required meta tags -->
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+
+    <!-- Bootstrap CSS -->
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.0-beta1/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-giJF6kkoqNQ00vy+HMDP7azOuL0xtbfIcaT9wjKHr8RbDVddVHyTfAAsrekwKmP1" crossorigin="anonymous">
+
+    <title>MySQL Sample Database Demo</title>
+  </head>
+  <body>
+    <nav class="navbar navbar-expand-lg navbar-light bg-light">
+      <div class="container-fluid">
+        <a class="navbar-brand" href="#">Database List</a>
+      </div>
+    </nav>
+    <div class="container">
+      <h3>city</h3>
+      <table class="table table-primary">
+        <thead>
+          <th>ID</th>
+          <th>Name</th>
+          <th>CountryCode</th>
+          <th>District</th>
+          <th>Population</th>
+        </thead>
+      </table>
+    </div>
+    <div class="container">
+      <h3>country</h3>
+      <table class="table table-secondary">
+        <thead>
+          <th>Code</th>
+          <th>Name</th>
+          <th>Continent</th>
+          <th>Region</th>
+          <th>SurfaceArea</th>
+          <th>IndepYear</th>
+          <th>Population</th>
+          <th>LocalName</th>
+          <th>HeadOfState</th>
+          <th>Capital</th>
+          <th>Code2</th>
+        </thead>
+      </table>
+    </div>
+    <div class="container">
+      <h3>countrylanguage</h3>
+      <table class="table table-info">
+        <thead>
+          <th>CountryCode</th>
+          <th>Language</th>
+          <th>CountryCode</th>
+          <th>IsOfficial</th>
+          <th>Percentage</th>
+        </thead>
+      </table>
+    </div>
+
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.0.0-beta1/dist/js/bootstrap.bundle.min.js" integrity="sha384-ygbV9kiqUc6oa4msXn9868pTtWMgiQaeYH7/t7LECLbyPA2x65Kgf80OJFdroafW" crossorigin="anonymous"></script>
+  </body>
+</html>
 
