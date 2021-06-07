c7f041db393bdf95c5b130a90b965afb7f27a5e1
ksbitbusiness <83622223+ksbitbusiness@users.noreply.github.com>
2021-06-07 16:12:41 +0900

    Update city-input.php

--- html/city-input.php
+++ html/city-input.php
@@ -1,1 +1,47 @@
+<!doctype html>
+<html lang="en">
+  <head>
+    <!-- Required meta tags -->
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
 
+    <!-- Bootstrap CSS -->
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.0-beta1/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-giJF6kkoqNQ00vy+HMDP7azOuL0xtbfIcaT9wjKHr8RbDVddVHyTfAAsrekwKmP1" crossorigin="anonymous">
+
+    <title>MySQL Sample Database Demo</title>
+  </head>
+  <body>
+    <nav class="navbar navbar-expand-lg navbar-light bg-light">
+      <div class="container-fluid">
+        <a class="navbar-brand" href="#">Database List</a>
+      </div>
+    </nav>
+    <div class="container">
+      <h3>city 登録</h3>
+      <form class="row g-3">
+        <div class="col-12">
+          <label for="Name" class="form-label">Name</label>
+          <input type="text" class="form-control" id="Name">
+        </div>
+        <div class="col-12">
+          <label for="CountryCode" class="form-label">CountryCode</label>
+          <input type="text" class="form-control" id="CountryCode">
+        </div>
+        <div class="col-12">
+          <label for="District" class="form-label">District</label>
+          <input type="text" class="form-control" id="District">
+        </div>
+        <div class="col-12">
+          <label for="Population" class="form-label">Population</label>
+          <input type="number" class="form-control" id="Population">
+        </div>
+        <div class="col-12">
+          <button type="submit" class="btn btn-info">
+            登録
+          </button>
+        </div>
+      </form>
+    </div>
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.0.0-beta1/dist/js/bootstrap.bundle.min.js" integrity="sha384-ygbV9kiqUc6oa4msXn9868pTtWMgiQaeYH7/t7LECLbyPA2x65Kgf80OJFdroafW" crossorigin="anonymous"></script>
+  </body>
+</html>
